ffaefd306dad49118f7c7aa3c10be8e69aca2630
Leonardo França <leofrancal17@gmail.com>
2026-03-13 10:54:33 -0300

    chore: enhance next.config.mjs with security headers and optimizations

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,30 @@ const withPWA = withPWAInit({
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default withPWA(nextConfig);
